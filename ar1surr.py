be12b4641d53eb6a325fff61ddefaded91f4e3f8
Sandip Varkey George <sandip.varkey@gmail.com>
2020-06-15 13:40:20 +0200

    Update ar1surr.py

--- ar1surr.py
+++ ar1surr.py
@@ -1,4 +1,9 @@
+####Created by Sandip Varkey George####
+###contains functions to generate ar1 surrogates###
+
 import numpy as np
+##The acf1 fucntion takes the time array, response array and bin size (sampling time) 
+##as the input variables and returns the autocorelation at lag 1
 def acf1(t,x,bs):
 	mx=np.mean(x)
 	denom=0.
@@ -19,9 +24,12 @@ def acf1(t,x,bs):
 		denom=denom+(x[i]*x[i])
 	rk=rk/denom
 	return (rk)
+##ar1 returns the next value in an ar1 process when the previous value, correlation coefficient and variance are entered 
 def ar1(x0,rk,var):
 	sd=np.sqrt(var)
 	return rk*x0+np.random.normal(0,sd)
+##acf1surrogates accepts a time series with seperate time and amplitude inputs and returns a surrogate dataset with 
+##the same autocorrelation at lag 1 and varaince structure as the original time series
 def acf1surr(t,x,bs):
 	import random
 	rk=acf1(t,x,bs)
